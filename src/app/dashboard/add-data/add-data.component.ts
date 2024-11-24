309e64f97e097512f0bf0c1151bb756e02ca6f5f
Farumai <emilidikandt@gmail.com>
2024-11-24 07:57:18 +0100

    feat: Checkbox + E-mail added

--- src/app/dashboard/add-data/add-data.component.ts
+++ src/app/dashboard/add-data/add-data.component.ts
@@ -3,10 +3,12 @@ import { SharedModule } from '../../shared/shared.module';
 import { Validators, FormBuilder } from '@angular/forms';
 import { StoreService } from '../../shared/store.service';
 import { BackendService } from '../../shared/backend.service';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-add-data',
   standalone: true,  // standalone-Komponente
-  imports: [SharedModule],  // Import der benötigten Module
+  imports: [SharedModule, MatCheckboxModule, MatInputModule],  // Import der benötigten Module
   templateUrl: './add-data.component.html',
   styleUrls: ['./add-data.component.css']
 })
@@ -18,9 +20,21 @@ export class AddDataComponent implements OnInit {
   ngOnInit(): void {
     this.registrationForm = this.formbuilder.group({
       name: ['', [Validators.required]],
+      birthdate: [null, Validators.required],
       courseId: ['', Validators.required],
-      birthdate: [null, Validators.required]
-    })
+      subscribeNewsletter: [false],
+      email: ['', Validators.email]
+    });
+
+    this.registrationForm.get('subscribeNewsletter')?.valueChanges.subscribe((isSubscribed: boolean) => {
+      const emailControl = this.registrationForm.get('email');
+      if (isSubscribed) {
+        emailControl?.setValidators([Validators.required, Validators.email]);
+      } else {
+        emailControl?.clearValidators();
+      }
+      emailControl?.updateValueAndValidity();
+    });
   }
 
   onSubmit() {
